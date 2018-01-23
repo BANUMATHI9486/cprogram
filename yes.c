e1d7e406c62677a47eb142403bd2e5d846ca9141
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-01-23 18:17:15 +0530

    Create yes.c

---
+++ yes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the no \n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {printf("yes");
+    }
+    else{
+        printf("no");
+        
+    }
+}
