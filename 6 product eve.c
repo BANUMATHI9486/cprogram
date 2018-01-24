02dd19527cce7a9ae8ec1ca62a000184d4d3ed45
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-01-24 16:09:06 +0530

    Create 6 product eve.c

---
+++ 6 product eve.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,d;
+    printf("enter the no ");
+    scanf("%d%d",&a,&b);
+    d=a*b;
+    if(d%2==0)
+    {
+        printf("even");
+        
+    }
+    else
+    {
+        printf("odd");
+}
+}
