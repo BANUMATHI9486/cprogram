2dba2bb725f4c804231857b70e93ef99197305ec
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-01-25 18:36:14 +0530

    Create 5 range10.c

---
+++ 5 range10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the no");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(n<=10)
+        {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    }
+}
