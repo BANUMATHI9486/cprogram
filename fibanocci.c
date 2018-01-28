d6db2ca3e31aef3cfb456ccdf85997814701b4d1
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-01-28 15:51:24 +0530

    Update fibanocci.c

--- fibanocci.c
+++ fibanocci.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 void main()
 {
-    int s=1,f=0,n;
+    int s=1,f=0,n,i,b;
     printf("enter the nos\n");
-    scanf("%d%d",&s,&f);
+    scanf("%d",&b);
+    printf("%d %d",f,s);
+    for(i=2;i<=b;i++)
     {
     n=f+s;
     f=s;
     s=n;
-    }
-    printf("the fibanocci series is %d",n);
+    printf(" %d",n);
+}
 }
