fb6e044bd3fb0dc2e8aeccd21b93d52c9ac240c5
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-02-07 17:01:49 +0530

    Create 3yes.c

---
+++ 3yes.c
@@ -0,0 +1,19 @@
+int main()
+{
+	char a[10];
+	int i;
+	printf("enter the string\n");
+	scanf("%s",&a);
+	for(i=0;i<=65;i++)
+	{
+	if(a<65)
+	{
+		printf("yes");
+		
+	}
+	else
+	{
+		printf("no");
+	}
+}
+}
