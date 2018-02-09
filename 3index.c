a142a4a77d8d5ef0b4b5c8697de968c72318bab5
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-02-09 16:58:53 +0530

    Create 3index.c

---
+++ 3index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a[10];
+int n,i;
+printf("enter the no\n");
+for(i=0;i<=n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+printf(" %d\t %d\n",a[i],i);
+}
