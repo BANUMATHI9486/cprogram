3dc7a97e61b545ad2fa6167a985af1b796890c0c
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-02-28 15:46:41 +0530

    Create 8 divisible of 13.c

---
+++ 8 divisible of 13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    int b;
+    printf("enter the number\n");
+    scanf("%d",&b);
+    if(b%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+        
+    }
+}
