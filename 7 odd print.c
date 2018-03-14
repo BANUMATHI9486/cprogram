503394d455946dc1a06268e147340ce010c50f18
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-03-14 15:54:34 +0530

    Create 7 odd print.c

---
+++ 7 odd print.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,r,rev=0;
+	printf("enter the variable\n");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		rev=(rev*10)+r;
+		n=n/10;
+	}
+	n=rev;
+	while(n!=0)
+	{
+		r=n%10;
+		if(r%2==1)
+		{
+		printf("%d ",r);
+		}
+		n=n/10;
+	}
+	return 0;
+}
