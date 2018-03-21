b1314fac1f03b01639764745b1cb08288abe553f
BANUMATHI9486 <31499239+BANUMATHI9486@users.noreply.github.com>
2018-03-22 03:22:57 +1400

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int p,t,r,fv;
+    printf("enter the numbers\n");
+    scanf("%d%d%d",&p,&t,&r);
+    fv=(p*t*r)/100;
+    printf("floor value is %d",fv);
+}
